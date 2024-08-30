3dac5d770ec8597fad094884b80013c7b6d0f19d
Shelton Foust <sheltonfoust@gmail.com>
2024-08-30 14:24:26 -0500

    Added Domain Classes

---
+++ SocialWorkApp.Domain/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialWorkApp.Domain
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Report> Reports { get; set; }
+        public DateOnly YearBegin { get; set; }
+        public DateOnly YearEnd { get; set; }
+    }
+
+}
