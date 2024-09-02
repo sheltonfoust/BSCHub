9f1da7fe89702ad14e8fbb871b4ec41bfae1844e
Shelton Foust <sheltonfoust@gmail.com>
2024-09-02 13:40:51 -0500

    Added Test for Client Report Info

---
+++ SocialWorkApp.Tests/Clients/ClientReportInfo.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SocialWorkApp.Core.Tests.Reports
+{
+    internal class ClientReportInfo
+    {
+        public ClientReportInfo()
+        {
+        }
+
+        public DateOnly ISP_MeetingDate { get; set; }
+        public bool isSevere { get; set; }
+        public bool hasBCIP { get; set; }
+        public bool hasPPMP { get; set; }
+        public bool hasRMP { get; set; }
+    }
+}
---
+++ SocialWorkApp.Tests/Clients/InputInformationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialWorkApp.Core.Tests.Reports
+{
+    [TestFixture]
+    public class InputInformationTests
+    {
+        [Test]
+        public void ShouldModifyClient()
+        {
+            var client = new Client();
+            var reportInfo = new ClientReportInfo()
+            {
+                ISP_MeetingDate = new DateOnly(2025, 3, 15),
+                isSevere = true,
+                hasBCIP = false,
+                hasPPMP = true,
+                hasRMP = false
+            };
+
+            client.InputReportInfo(reportInfo);
+
+            Assert.That(client.ISP_MeetingDate, Is.EqualTo(new DateOnly(2025, 3, 15)));
+            Assert.That(client.IsSevere, Is.EqualTo(true));
+            Assert.That(client.HasBCIP, Is.EqualTo(false));
+            Assert.That(client.HasPPMP, Is.EqualTo(true));
+            Assert.That(client.HasRMP, Is.EqualTo(false));
+
+        }
+    }
+}
