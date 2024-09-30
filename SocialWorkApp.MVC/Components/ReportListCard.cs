84ea2675664ddd9863ccd10c690190cd1de6a192
Shelton Foust <sheltonfoust@gmail.com>
2024-09-30 13:01:39 -0500

    Fix Report Accordion

---
+++ SocialWorkApp.MVC/Components/ReportListCard.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialWorkApp.Domain.Reports;
+using SocialWorkApp.MVC.ViewModels;
+
+namespace SocialWorkApp.MVC.Components
+{
+    public class ReportListCard : ViewComponent
+    {
+        public IViewComponentResult Invoke(string name, IEnumerable<Report> reports)
+        {
+            var viewModel = new ReportListCardViewModel(name, reports);
+            return View(viewModel);
+        }
+    }
+}
---
+++ SocialWorkApp.MVC/ViewModels/ReportListCardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SocialWorkApp.Domain.Reports;
+
+namespace SocialWorkApp.MVC.ViewModels
+{
+    public class ReportListCardViewModel
+    {
+        public string Name { get; }
+        public string NameNoSpaces => Name.Replace(" ", string.Empty);
+        public IEnumerable<Report> Reports { get;}
+        public ReportListCardViewModel(string name, IEnumerable<Report> reports)
+        {
+            Name = name;
+            Reports = reports;
+        }
+    }
+}
